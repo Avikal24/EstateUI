57516279d4e7c01953991a1c3d6e4ef683e3f3e4
Avikal Shukla <avikalshukla04@gmail.com>
2025-11-07 17:05:53 +0530

    Created responsive Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import "./layout.scss"
+import Navbar from "./components/navbar/Navbar.jsx"
+import HomePage from "./routes/homePage/homePage.jsx"
+
 function App() {
   return (
-    <div>Hello World</div>
+    <div className="layout">
+      <div className="navbar">
+           <Navbar/>
+      </div>
+    <div className="content">
+      <HomePage />
+    </div>
+   </div>
   )
 }
 
